b23fae61ea40f0cf7d047d40d26a1917608a0544
Antônio Lemos <antonio.lemos@beyondanalytics.io>
2025-07-06 18:12:15 -0300

    init

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Example Go development environment for Zero to Nix";
+
+  inputs = {
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0";
+  };
+
+  # Flake outputs
+  outputs = { self, nixpkgs }:
+    let
+      allSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+
+      # Helper to provide system-specific attributes
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      # Development environment output
+      devShells = forAllSystems ({ pkgs }: {
+        default = pkgs.mkShell {
+          # The Nix packages provided in the environment
+          packages = with pkgs; [
+            zsh
+            go
+            fnm
+            starship
+          ];
+
+          shellHook = ''
+            exec "${pkgs.zsh}/bin/zsh"
+          '';
+        };
+      });
+    };
+}
