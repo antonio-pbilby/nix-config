c464e6fb7545a9f747c20f9b75f06ce695c2db17
Antônio Lemos <antonio.lemos@beyondanalytics.io>
2025-07-06 19:17:56 -0300

    remove macOS

--- flake.nix
+++ flake.nix
@@ -11,8 +11,6 @@
       allSystems = [
         "x86_64-linux" # 64-bit Intel/AMD Linux
         "aarch64-linux" # 64-bit ARM Linux
-        "x86_64-darwin" # 64-bit Intel macOS
-        "aarch64-darwin" # 64-bit ARM macOS
       ];
 
       # Helper to provide system-specific attributes
@@ -41,8 +39,6 @@
             grep -qxF 'eval "$(fnm env --use-on-cd --shell zsh)"' $HOME/.zshrc || echo 'eval "$(fnm env --use-on-cd --shell zsh)"' >> $HOME/.zshrc
 
             exec "${pkgs.zsh}/bin/zsh"
-            echo $HOME
-            cd $HOME
           '';
         };
       });
